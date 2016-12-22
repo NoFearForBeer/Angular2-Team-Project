94f7ceae433f9213082ab0cad198fe4e5cfbf43a
medeaohm <nia_omerska@abv.bg>
2016-12-23 00:27:03 +0200

    added alert and bootstrap used

--- source/app/app.module.ts
+++ source/app/app.module.ts
@@ -10,13 +10,30 @@ import { HomeContainer } from './components/containers/home/home-contianer';
 import { HttpModule } from '@angular/http';
 import { CookieService  } from '../node_modules/angular2-cookie/services/cookies.service';
 
+ 
+import { AlertComponent } from './components/alert-component';
+import { AlertService } from './services//alert-service';
+ 
 @NgModule({
     imports: [
-        BrowserModule, FormsModule, HttpModule
+        BrowserModule,
+        FormsModule,
+        HttpModule,
     ],
-    declarations: [AppComponent, LoginForm, HomeContainer],
-    providers: [AuthService, ApiService, CookieService],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeContainer,
+        LoginForm,
+    ],
+    providers: [
+        AuthService,
+        ApiService,
+        CookieService,
+    ],
     bootstrap: [AppComponent]
 })
-
+ 
 export class AppModule { }
+
+
