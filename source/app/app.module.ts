f86d20d9469cbaf6f20b0c048f675ad9f6b6ca1a
medeaohm <nia_omerska@abv.bg>
2016-12-24 16:56:32 +0200

    auth guard implemented

--- source/app/app.module.ts
+++ source/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { CookieService } from '../node_modules/angular2-cookie/services/cookies.service';
+
+import { HomeContainer } from './components/containers/home/home-container';
+
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
+
 import { LoginForm } from './components/login/loginForm-component';
 import { RegisterComponent } from './components/register/register-component';
 import { MapComponent } from './components/map/map-component';
-import { FormsModule } from '@angular/forms';
+import { AlertComponent } from './components/alert/alert-component';
+
 import { AuthService } from './services//auth-service';
 import { UserService } from './services//user-service';
 import { ApiService } from './services//api-service';
-import { HomeContainer } from './components/containers/home/home-container';
-import { HttpModule } from '@angular/http';
-import { CookieService  } from '../node_modules/angular2-cookie/services/cookies.service';
-import { BuyTicketModule } from './tickets';
- 
-import { AlertComponent } from './components/alert/alert-component';
 import { AlertService } from './services//alert-service';
+
+import { BuyTicketModule } from './tickets';
+
+import { AuthGuard } from './guards/auth-guard';
  
 @NgModule({
     imports: [
@@ -37,6 +43,7 @@ import { AlertService } from './services//alert-service';
     ], // to be refactored to modules
     
     providers: [
+        AuthGuard,
         AuthService, 
         UserService,
         ApiService, 
