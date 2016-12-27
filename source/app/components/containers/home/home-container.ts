0e18fbf7e01699b43baebfc707c4d2a6e937f669
M-Yankov <pointblank12345@abv.bg>
2016-12-27 21:31:10 +0200

    Added link to user profile. Home container removed unused code.

--- source/app/components/containers/home/home-container.ts
+++ source/app/components/containers/home/home-container.ts
@@ -1,45 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth-service';
 import { ApiService } from '../../../services/api-service';
-import { Response } from '@angular/http';
-import { User } from '../../../models/user';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'home-container',
     templateUrl: './home-container.html'
 })
 export class HomeContainer {
-    currentUser: User;
-    userName: String = '<UserName>';
-    result: string = 'Test';
+    userName: string = '<UserName>';
 
-    constructor(private authService: AuthService, private apiService: ApiService) { }
+    constructor(private authService: AuthService,
+                private apiService: ApiService,
+                private router: Router) { }
 
     ngOnInit() {
-       // TODO: get Info!
-    }
-
-    // TODO: Delete
-    checkAuthorization(): void {
-        this.apiService.get('/Values')
-            .subscribe(json => this.result = json, err => this.result = err);
-    }
-
-    // TODO: Delete
-    getToken(user: any): void {
-        let { username, password } = user;
-        console.log(user);
-        this.authService.login(username, password).subscribe(
-            (response: Response) => {
-               // console.log(response.json());
-            },
-             (error: any) => {
-                 console.log(error);
-            }
-        );
+        // TODO: known bug this should be updated on login !
+       this.userName = this.authService.getLoggedUser().userName;
     }
 
     logout() {
         this.authService.logout();
+        this.router.navigate(['/']);
     }
 }
