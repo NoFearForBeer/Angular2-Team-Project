8be1efce57bb2d9dc4025df060583359195a53f7
medeaohm <nia_omerska@abv.bg>
2017-01-02 13:08:59 +0200

    typo in ticket-detail component fixed

--- app/tickets/ticket-detail/ticket-detail-component.ts
+++ app/tickets/ticket-detail/ticket-detail-component.ts
@@ -2,10 +2,10 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Ticket } from '../../models';
 
 @Component({
-    selector: '[ticket-detail-comopnent]',
-    templateUrl: './ticket-detail-comopnent.html'
+    selector: '[ticket-detail-component]',
+    templateUrl: './ticket-detail-component.html'
 })
-export class TicketDetailCompoennt {
+export class TicketDetailComponent {
     @Input() ticketModel: Ticket;
 
     @Output() ticketActivatedDetail = new EventEmitter();
