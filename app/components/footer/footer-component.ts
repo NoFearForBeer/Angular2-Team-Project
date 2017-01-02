7de9880366ad071c6b50cefc065d2f79c7816a3b
medeaohm <nia_omerska@abv.bg>
2017-01-02 13:06:38 +0200

    typos fixed

--- app/components/footer/footer-component.ts
+++ app/components/footer/footer-component.ts
@@ -6,4 +6,4 @@ import { Component } from '@angular/core';
     templateUrl: './footer-component.html',
     styleUrls: [ './footer-component.css']
 })
-export class FooterCompoenent { }
+export class FooterComponent { }
