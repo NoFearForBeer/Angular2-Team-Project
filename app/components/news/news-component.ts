a16b05d75b8294181b719b83b2481b03176b38d4
medeaohm <nia_omerska@abv.bg>
2017-01-02 18:25:10 +0200

    news and comments UI

--- app/components/news/news-component.ts
+++ app/components/news/news-component.ts
@@ -5,9 +5,9 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import 'rxjs';
 
-import { AuthService } from '../../services/auth-service';
-import { NewsService } from '../../services/news-service';
-import { AlertService } from '../../services/alert-service';
+import { AuthService, NewsService, AlertService } from '../../services/index';
+
+import { SortPipe } from '../../pipes/index';
 
 import { flyInOut } from '../../animations/flyInOut-animation';
 
@@ -16,14 +16,14 @@ import { News } from '../../models/news';
 @Component({
   selector: 'news-component',
   templateUrl: './news-component.html',
-  providers: [NewsService],
+  providers: [NewsService, SortPipe],
   animations: [flyInOut()]
 })
 
 
 export class NewsComponent implements OnInit {
 
-    news: News[];
+    news: News[] = [];
     error: any;
 
     constructor(
@@ -31,7 +31,8 @@ export class NewsComponent implements OnInit {
         private router: Router,
         private newsService: NewsService,
         private authService: AuthService,
-        private alertService: AlertService
+        private alertService: AlertService,
+        private sort: SortPipe
     ) { }
 
     ngOnInit() {
